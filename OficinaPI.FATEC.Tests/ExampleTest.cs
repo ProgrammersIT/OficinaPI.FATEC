dc5d8ce10d2f0fb9453ece08f5e8545f670b0a9e
Lázaro Menezes <Lzaro Menezes>
2016-04-06 14:12:02 -0300

    starter template

---
+++ OficinaPI.FATEC.Tests/ExampleTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace OficinaPI.FATEC.Tests
+{
+    public class ExampleTest
+    {
+        [Test]
+        public void FailingTest()
+        {
+            Assert.Fail("Failling Test");
+        }
+
+        [Test]
+        public void PassingTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
